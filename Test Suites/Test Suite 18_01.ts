a4a3e92cf252451d1e25f61af7d7f9b77995ce24
thanhnle <93516707+thanhnle@users.noreply.github.com>
2022-01-18 18:07:50 +0700

    18_01

---
+++ Test Suites/Test Suite 18_01.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite 18_01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>6d28d78e-7d63-4dd1-8667-aba604c94c75</testSuiteGuid>
+   <testCaseLink>
+      <guid>49639649-2a32-4072-9341-8a62f727db84</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Form</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
