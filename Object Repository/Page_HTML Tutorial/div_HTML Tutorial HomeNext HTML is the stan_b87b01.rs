5c4a49fbaf383efc6e98b35ef563f056c6f9727b
thanhnle <93516707+thanhnle@users.noreply.github.com>
2022-03-17 14:35:09 +0700

    add

---
+++ Object Repository/Page_HTML Tutorial/div_HTML Tutorial HomeNext HTML is the stan_b87b01.rs
@@ -0,0 +1,382 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_HTML Tutorial HomeNext HTML is the stan_b87b01</name>
+   <tag></tag>
+   <elementGuidId>1ddcefc0-c349-44e6-824a-2670d9618bfb</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='main']</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>#main</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>w3-col l10 m12</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>main</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+      
+        
+
+        
+        
+        
+
+      
+HTML Tutorial
+
+❮ Home
+Next ❯
+
+
+
+HTML is the standard markup language for Web pages.
+With HTML you can create your own Website.
+HTML is easy to learn - You will enjoy it!
+Study our free HTML Tutorial »
+Watch our Video Tutorial »NEW
+
+
+
+Easy Learning with HTML &quot;Try it Yourself&quot;
+With our &quot;Try it Yourself&quot; editor, you can edit the HTML code and view the 
+result:
+
+
+Example
+
+&lt;!DOCTYPE html>
+&lt;html>&lt;head>&lt;title>Page Title&lt;/title>
+&lt;/head>&lt;body>&lt;h1>This is a Heading&lt;/h1>&lt;p>This is a paragraph.&lt;/p>
+&lt;/body>&lt;/html>
+ 
+Try it Yourself »
+
+Click on the &quot;Try it Yourself&quot; button to see how it works.
+
+
+HTML Examples
+In this HTML tutorial, you will find more than 200 examples. With our online 
+&quot;Try it Yourself&quot; editor, you can edit and test each example yourself!
+Go to HTML Examples!
+
+
+
+
+
+  
+  
+
+
+HTML Exercises
+This HTML tutorial also contains nearly 100 HTML exercises.
+
+Test Yourself With Exercises
+
+Exercise:
+Add a &quot;tooltip&quot; to the paragraph below with the text &quot;About W3Schools&quot;.
+
+&lt;p =&quot;About W3Schools&quot;>W3Schools is a web developer's site.&lt;/p>
+
+
+Submit Answer »
+Start the Exercise
+
+
+
+HTML Quiz Test
+Test your HTML skills with our HTML Quiz!
+Start HTML Quiz!
+
+
+HTML References
+At W3Schools you will find complete references about HTML elements, 
+attributes, events, color names, entities, character-sets, URL encoding, 
+language codes, HTTP messages, browser support, and more:
+
+
+
+
+  HTML Elements
+
+
+
+  Browser Support
+
+
+
+  Attributes
+
+
+
+  Global Attributes
+
+
+
+  Event Attributes
+
+
+
+  Color Names
+
+
+
+  Canvas
+
+
+
+  Audio/Video DOM
+
+
+
+  Character Sets
+
+
+
+  URL Encoding
+
+
+
+  Language Codes
+
+
+
+  Country Codes
+
+
+
+  HTTP Messages
+
+
+
+  Px to Em Converter
+
+
+
+  Keyboard Shortcuts
+
+
+
+
+
+  
+
+#w3_cert_badge {
+  position: absolute;
+  right: 5%;
+  width: 220px;
+  transform: rotate(10deg);
+  bottom: -20%;
+}
+
+#w3_cert_arrow {
+  position: absolute;
+  right: 220px;
+  width: 220px;
+  transform: rotate(10deg);
+  bottom: 0;
+  z-index: 1;
+}
+
+#getdiploma {
+  position: relative;
+  padding: 0 60px 50px 60px;
+  margin-bottom: 125px;
+  border-radius: 16px;
+  background-color: #282A35;
+  color: #FFC0C7;
+  font-family: 'Source Sans Pro', sans-serif;
+}
+
+#getdiploma h2 {
+  font-size:50px;
+  margin-top: 1em;
+  margin-bottom: 1em;
+  font-family: 'Source Sans Pro', sans-serif
+}
+
+#getdiploma p {
+  font-size: 42px;
+  margin-top: 1em;
+  margin-bottom: 1em;
+  font-family: 'Source Sans Pro', sans-serif
+}
+
+#getdiploma a {
+  border-radius: 50px;
+  mxargin-top: 50px;
+  font-size: 18px;
+  background-color: #04AA6D;
+  padding: 17px 55px
+}
+
+#getdiploma a:hover,
+#getdiploma a:active {
+  background-color: #059862 !important;
+}
+
+
+@media screen and (max-width: 1442px) {
+  #w3_cert_arrow {
+    right: 212px;
+    bottom: -15px;
+  }
+}
+
+
+@media screen and (max-width: 1202px) {
+  #w3_cert_arrow {
+    display: none;
+  }
+}
+
+@media screen and (max-width: 992px) {
+  #w3_cert_arrow {
+    display: block;
+  }
+}
+
+
+@media screen and (max-width: 800px) {
+  #w3_cert_arrow {
+    display: none;
+  }
+  #getdiploma h2 {
+    font-size: 44px;
+  }
+  #getdiploma p {
+    font-size: 30px;
+  }
+
+  #getdiploma a {
+    width: 100%;
+  }
+  #w3_cert_badge {
+    top: -16px;
+    right: -8px;
+    width: 100px;
+  }
+  #getdiploma  {
+    margin-bottom: 55px;
+  }
+}
+
+
+
+
+Kickstart your career
+Get certified by completing the HTML course
+
+var foldername;
+function getCourseFolder() {
+  var pathname = window.location.pathname;
+  if (pathname.substr(0,1) == &quot;/&quot;) {pathname = pathname.substr(1);}
+  pos = pathname.indexOf(&quot;/&quot;);
+  foldername = pathname.substr(0, pos);
+  foldername = foldername.toUpperCase();
+  if (foldername == &quot;JS&quot;) foldername = &quot;JavaScript&quot;;
+  if (foldername == &quot;QUIZTEST&quot; || foldername == &quot;EXERCISES&quot;) {
+    document.getElementById(&quot;hey&quot;).innerText = &quot;a&quot;;
+  }
+  if (foldername == &quot;QUIZTEST&quot; || foldername == &quot;EXERCISES&quot;) foldername = &quot;&quot;;
+  if (foldername == &quot;CS&quot;) foldername = &quot;C#&quot;;
+  if (foldername == &quot;CYBERSECURITY&quot;) foldername = &quot;Cyber Security&quot;;  
+  if (foldername == &quot;CPP&quot;) foldername = &quot;C++&quot;;
+  document.getElementById(&quot;coursetopic&quot;).innerText = foldername;
+}
+getCourseFolder();
+
+Get certified
+
+
+
+
+
+.cls-1{fill:#04aa6b;}.cls-2{font-size:23px;}.cls-2,.cls-3,.cls-4{fill:#fff;}.cls-2,.cls-3{font-family:RobotoMono-Medium, Roboto Mono;font-weight:500;}.cls-3{font-size:20.08px;}w3schools CERTIFIED  .  2022
+
+
+
+
+
+
+
+
+❮ Home
+Next ❯
+
+
+
+
+</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;main&quot;)</value>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:attributes</name>
+      <type>Main</type>
+      <value>//div[@id='main']</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='belowtopnav']/div/div</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Keyboard Shortcuts'])[1]/following::div[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='PX to EM Converter'])[1]/following::div[3]</value>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div[7]/div/div</value>
+   </webElementXpaths>
+</WebElementEntity>
