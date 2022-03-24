b9c83cb1831ab37ae53f3670f795c214b3d54e42
thanhnle <93516707+thanhnle@users.noreply.github.com>
2022-03-24 17:34:44 +0700

    add TS

---
+++ Test Suites/Big TS.ts
@@ -0,0 +1,38 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Big TS</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>e8e7ac30-9c7a-4da1-98ca-e2a9394423b7</testSuiteGuid>
+   <testCaseLink>
+      <guid>2a16edcc-7874-4ae7-af73-1a486d2cd248</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/apple</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b57283a3-8874-4109-b179-55bc82285d57</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Form</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>b9c7357c-4f0f-495d-9a76-309f9370e0fd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test Vingroup</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e8bf9a6b-8f5d-43f3-93a9-5e13d2dd4587</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/w3</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
