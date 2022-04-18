94f730ed845bf52ebcad3644d21723ac57580fc1
thanhnle <93516707+thanhnle@users.noreply.github.com>
2022-04-18 11:14:47 +0700

    add tsc with many browser type

---
+++ Test Suites/TS_w3.ts
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS_w3</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>62b84178-64d8-4eb6-a1e8-965fdf706e03</testSuiteGuid>
+   <testCaseLink>
+      <guid>cc7b0c73-03ec-4f82-86e3-4a612436297b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/w3</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
