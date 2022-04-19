270876821fe3807f32585cf85a9b80f003fcad6f
thanhnle <93516707+thanhnle@users.noreply.github.com>
2022-04-19 10:38:10 +0700

    add more

--- Test Suites/w3 TSC - 8 browser type.ts
+++ Test Suites/w3 TSC - 8 browser type.ts
@@ -1,7 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <TestSuiteCollectionEntity>
    <description></description>
-   <name>w3 TSC</name>
+   <name>w3 TSC - 8 browser type</name>
    <tag></tag>
    <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
