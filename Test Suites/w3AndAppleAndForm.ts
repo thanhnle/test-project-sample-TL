a6cbbdaa48cf23bc2c83557774737e581c6f6705
thanh.nle <thanh.nle@katalon.com>
2022-06-16 10:36:02 +0700

    add testlistener

---
+++ Test Suites/w3AndAppleAndForm.ts
@@ -0,0 +1,32 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>w3AndAppleAndForm</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>003f14f1-ab1e-425b-8bfa-72c13a145d5a</testSuiteGuid>
+   <testCaseLink>
+      <guid>68e85906-285f-4e14-810d-7c2ed6e1b5bf</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/apple</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>38173593-c4e2-4299-89b6-479e9790ec15</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/w3</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ff28162f-e53c-428b-8166-9e33ac7fb98c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Form</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
