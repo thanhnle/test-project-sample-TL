59d6bb3828c94c1f6ca3c8463d5c10577f0ef747
Thanh Le <93516707+thanhnle@users.noreply.github.com>
2023-05-31 13:52:03 +0700

    third commit

---
+++ test-suites/g5/1P-1F G5 TS.ts
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<TestSuiteEntity>
+    <name>1P-1F G5 TS</name>
+    <testSuiteGuid>a3e4ceae-e4a6-4205-94ed-0714d004863f</testSuiteGuid>
+    <testCaseLink>
+        <testCaseId>test-cases/g5/Failed G5 TC</testCaseId>
+        <guid>f54a8d28-ab84-4732-bdb5-77eac2c59064</guid>
+    </testCaseLink>
+    <testCaseLink>
+        <testCaseId>test-cases/g5/Passed G5 TC</testCaseId>
+        <guid>1ef73b5e-d82c-4174-ac8a-60a55048663e</guid>
+    </testCaseLink>
+</TestSuiteEntity>
